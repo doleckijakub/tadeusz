59e0ceb4096ba9bef818dea1a61c2f942bc8e670
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2026-04-22 14:39:21 +0200

    Implemented .gitignore in subcrates

--- tool_derive/target/debug/build/quote-96b2676a2dd45b46/build_script_build-96b2676a2dd45b46.d
+++
@@ -1,5 +0,0 @@
-/home/jacob/programming/tadeusz/tool_derive/target/debug/build/quote-96b2676a2dd45b46/build_script_build-96b2676a2dd45b46.d: /home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/jacob/programming/tadeusz/tool_derive/target/debug/build/quote-96b2676a2dd45b46/build_script_build-96b2676a2dd45b46: /home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
--- tool_derive/target/debug/deps/unicode_ident-659b360bc6df18f2.d
+++
@@ -1,6 +0,0 @@
-/home/jacob/programming/tadeusz/tool_derive/target/debug/deps/unicode_ident-659b360bc6df18f2.d: /home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/jacob/programming/tadeusz/tool_derive/target/debug/deps/libunicode_ident-659b360bc6df18f2.rmeta: /home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
-/home/jacob/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
